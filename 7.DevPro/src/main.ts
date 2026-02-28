83a55c7a191757b8c4dd02bc7c2a3a8f3457e68d
Jefferson Steven Espitia Florez <jeffer_espitia@hotmail.com>
2026-02-28 08:44:19 +0100

    Update-files-src

--- 7.DevPro/src/main.ts
+++ 7.DevPro/src/main.ts
@@ -1,26 +1,21 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from '@/counter'
+import { footer } from '@/components/footer/footer';
+import { home } from '@/pages/home/home';
+import './style.css';
 
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const setTemplate = () => {
+    return /*html*/ `
+    <div>
+        <main></main>
+        <app-footer></app-footer>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+    `;
+};
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const target = document.querySelector('#app');
+if (target) {
+    target.innerHTML = setTemplate();
+}
+
+footer();  
+home();
