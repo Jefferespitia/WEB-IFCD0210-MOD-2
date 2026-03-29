5c1fabbc1170a9abc1c678ad274c53fa395aa5ae
Jefferson Steven Espitia Florez <jeffer_espitia@hotmail.com>
2026-03-29 11:05:25 +0200

    Add-file-vite-env

--- 7.DevPro/src/pages/home/counter/counter.ts
+++ 7.DevPro/src/pages/home/counter/counter.ts
@@ -1,4 +1,4 @@
-import "./counter.css";
+import "../counter/counter.css";
 
 /*
     Vanilla Component
--- 7.DevPro/vite.config.ts
+++ 7.DevPro/vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, './src'), // 👈 importante el ./src
     },
   },
-});
+})
